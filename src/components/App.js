7a45891e0c87e211939695d0e9aa3b39057f26e2
Dvontre A Coleman <dvontrec@gmail.com>
2018-06-19 18:54:51 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import SearchBar from '../containers/search_bar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
