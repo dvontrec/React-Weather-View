50e81e84c64598a1e437eb794f755d79ab6e71c5
Dvontre A Coleman <dvontrec@gmail.com>
2018-06-19 20:24:35 -0400

    Setup basic form for search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component
 {
+	// constructor for the SearchBar component
+	constructor(props)
+	{
+		super(props)
+
+		// initializes state.term to be an empty string
+		this.state = { term: '' };
+
+		//sets the value of the word (this) to be bound to this searchbar object for the function onInputChange
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+
+	//function that changes the term to be equal to the user input 
+	onInputChange(event)
+	{
+		//changes the state to equal the user input
+		this.setState({ term:event.target.value });
+
+	}
+
+	onFormSubmit(event)
+	{
+		//prevents default form event from running
+		event.preventDefault();
+		//fetches weather data for the selected city
+		console.log(process.env.Test)
+	}
+
 	render()
 	{
+		// controlled field (the value of the input is set by the state)
 		return(
-			<form className="input-group">
-				<input />
+			<form onSubmit={ this.onFormSubmit } className="input-group">
+				<input 
+				placeholder="Get a 5 day forcast for a city"
+				className="form-control"
+				value={ this.state.term }
+				onChange={ this.onInputChange } />
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Search</button>
 				</span>
